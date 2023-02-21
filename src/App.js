a6917a7d05f02fa8f9b815ea90bb565dbe5996da
Ansari <44107987+786ansari@users.noreply.github.com>
2023-02-21 13:35:53 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 
 function App() {
   return (
+    <div className='main-image'>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        My love, you are the sunshine in my life that brightens up every day. I am grateful for your love and the joy that you bring into my life. You make me a better person, and I cherish every moment that we spend together. I want you to know that I love you more than words can express, and I will always be by your side through thick and thin. You are my soulmate, my best friend, and my everything. I love you now and forever.
+      <h4>I love u Mera Beta --  Aliya</h4>
       </header>
+    </div>
+    <div className='heart-img'>
+    <img src="../assets/img/love.png" alt="love-image" width="100px"/>
+    <img src="../assets/img/love.png" alt="love-image" width="100px"/>
+    <img src="../assets/img/love.png" alt="love-image" width="100px"/>
+    </div>
+    <div className='second-heart-img'>
+    <img src="../assets/img/love-birds.png" alt="love-image" width="100px"/>
+    <img src="../assets/img/love-birds.png" alt="love-image" width="100px"/>
+    <img src="../assets/img/love-birds.png" alt="love-image" width="100px"/>
+    </div>
     </div>
   );
 }
